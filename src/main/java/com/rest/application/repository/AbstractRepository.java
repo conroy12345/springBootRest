23ebbabe373a520fdcfa6c0dc9bf6b6f3910a6fa
conroy12345 <35980602+conroy12345@users.noreply.github.com>
2018-03-12 22:37:53 +0000

    Update AbstractRepository.java

--- src/main/java/com/rest/application/repository/AbstractRepository.java
+++ src/main/java/com/rest/application/repository/AbstractRepository.java
@@ -1,41 +1,9 @@
 package com.rest.application.repository;
 
-import java.io.Serializable;
+import java.util.List;
 
-import java.lang.reflect.ParameterizedType;
- 
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
- 
-public abstract class AbstractRepository<PK extends Serializable, T> {
-     
-    private final Class<T> persistentClass;
-     
-    @SuppressWarnings("unchecked")
-    public AbstractRepository(){
-        this.persistentClass =(Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
-    }
-     
-    @Autowired
-    private SessionFactory sessionFactory;
- 
-    protected Session getSession(){
-        return sessionFactory.getCurrentSession();
-    }
- 
-    @SuppressWarnings("unchecked")
-    public T getByKey(PK key) {
-        return (T) getSession().get(persistentClass, key);
-    }
- 
-    public void persist(T entity) {
-        getSession().persist(entity);
-    }
-    
-    protected Criteria createEntityCriteria(){
-        return getSession().createCriteria(persistentClass);
-    }
- 
+import com.liking.test.mode.DisLike;
+
+public interface DislikeRepository {
+public List<DisLike> processDislike();
 }
