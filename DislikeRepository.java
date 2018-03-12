24171858e7505518fc86c8c3db9c66c2ecfc3036
conroy12345 <35980602+conroy12345@users.noreply.github.com>
2018-03-12 23:18:44 +0000

    Delete DislikeRepository.java

--- DislikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.liking.test.repository;
-
-import java.util.List;
-
-import com.liking.test.mode.DisLike;
-
-public interface DislikeRepository {
-public List<DisLike> processDislike();
-}
