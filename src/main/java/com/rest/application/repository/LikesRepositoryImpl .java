b141ef914c6306769484413435da4dd5263a55e7
conroy12345 <35980602+conroy12345@users.noreply.github.com>
2018-03-12 22:44:25 +0000

    Create LikesRepositoryImpl .java

---
+++ src/main/java/com/rest/application/repository/LikesRepositoryImpl .java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.liking.test.mode.DisLike;
+import com.liking.test.mode.Likes;
+@Repository
+public class LikesRepositoryImpl implements LikeRepository {
+
+
+	@Override
+	public List<Likes> retrieveLikes() {
+		Likes like=new Likes(200);
+		List<Likes> likes=new ArrayList<>();
+		likes.add(like);
+		return likes;
+	}
+
+}
