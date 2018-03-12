ec093174c1017e2e0b1f77b5d10d8ce99ebf483f
conroy12345 <35980602+conroy12345@users.noreply.github.com>
2018-03-12 22:40:44 +0000

    Update and rename UserRepositoryImpl.java to LikeRepository.java

---
+++ src/main/java/com/rest/application/repository/LikeRepository.java
@@ -0,0 +1,12 @@
+package com.rest.application.repository;
+
+import java.util.List;
+
+import com.liking.test.mode.Likes;
+
+public interface LikeRepository {
+	
+public	List<Likes> retrieveLikes();
+
+}
+
